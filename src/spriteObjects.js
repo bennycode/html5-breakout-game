0d47c7a34f73b4c8e29e272e72120d126b777786
Benny Neugebauer <bn@bennyn.de>
2013-09-26 12:02:48 +0200

    Improved code formatting.

--- src/spriteObjects.js
+++ src/spriteObjects.js
@@ -1,58 +1,51 @@
-//--- The sprite object
-//Create new sprites by making new instances of this "spriteObject"
-//and customizing it.
-	
-var spriteObject =
-{
-	//The location and size of the image that represents the
-	//sprite on the tilesheet
+/**
+ * Create new sprites by making new instances of this "spriteObject" and customizing it. 
+ * 
+ * TODO: Don't pollute the global namespace!
+ */
+var spriteObject = {
+  // The location and size of the image that represents the sprite on the tilesheet.
   sourceX: 0,
   sourceY: 0,
   sourceWidth: 64,
   sourceHeight: 64,
-	
-	//The size and location of the sprite on the canvas
+  // The size and location of the sprite on the canvas
   height: 64,
   width: 64,
   x: 0,
   y: 0,
-	
-	//The sprite's velocity
-	vx: 0,
+  // The sprite's velocity
+  vx: 0,
   vy: 0,
-	
-	//Will the sprite be visible when it first loads?
+  // Will the sprite be visible when it first loads?
   visible: true,
-
-  //Getters 
-	//Any calculated in information that the main program might need
-	//from the sprite. In this example the getters 
-	//return the center x and y position of the sprite,
-	//as well as its half-width and half-height
-	centerX: function()
-  {
+  /**
+   * Getters:
+   * Any calculated in information that the main program might need from the sprite. 
+   * In this example the getters return the center x and y position of the sprite, 
+   * as well as its half-width and half-height.
+   */
+  centerX: function() {
     return this.x + (this.width / 2);
   },
-  centerY: function()
-  {
+  centerY: function() {
     return this.y + (this.height / 2);
   },
-  halfWidth: function()
-  {
+  halfWidth: function() {
     return this.width / 2;
   },
-  halfHeight: function()
-  {
+  halfHeight: function() {
     return this.height / 2;
   }
 };
-	
-//--- The message object
-//Used to position and display game messages.
-//Assign new values to the default properties when you create
-//a new message from this object	
-var messageObject =
-{
+
+/**
+ * Used to position and display game messages. Assign new values to the default 
+ * properties when you create a new message from this object.
+ * 
+ * TODO: Don't pollute the global namespace!
+ */
+var messageObject = {
   x: 0,
   y: 0,
   visible: true,
