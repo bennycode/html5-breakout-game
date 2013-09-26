4c9087f2b98cf4f95d591d70a72dd27955f56971
Benny Neugebauer <bn@bennyn.de>
2013-09-26 12:15:51 +0200

    Refactoring

--- src/sound.js
+++ src/sound.js
@@ -1,3 +1,4 @@
+// TODO: Don't pollute the global namespace!
 // Intialize the audio context
 var audioContext = new webkitAudioContext();
 
@@ -9,15 +10,17 @@ var soundObject = {
   volume: 1,
   pan: 0,
   loop: false,
-  // Can the sound be played many times simultaneously, like a sound effect,
-  // or is it a single instance sound, like background music, that should
-  // one have one instance running? If it's background music, set
-  // singleInstance to true. Later, in the "play" function, this object's playSound property
-  // will be set as the sound source buffer, so that it's global to this object,
-  // not temporary
+  /**
+   * Can the sound be played many times simultaneously, like a sound effect, 
+   * or is it a single instance sound, like background music, that should have 
+   * one instance running? 
+   * 
+   * If it's background music, set singleInstance to true. Later, in the "play" 
+   * function, this object's playSound property will be set as the sound source 
+   * buffer, so that it's global to this object.
+   */
   singleInstance: false,
   // Use immediate functions to initialze the audio context properties
-  // p.72 Stoyanov
   volumeConnection: (function() {
     return audioContext.createGainNode();
   }()),
@@ -30,41 +33,35 @@ var soundObject = {
   loadSound: (function() {
     return new XMLHttpRequest();
   }()),
-  // An initialization function that accepts a config object
-  // as its argument
-  initialize: function(config)
-  {
+  // An initialization function that accepts a config object as its argument
+  initialize: function(config) {
     // Set the source
-    if (config.source !== undefined)
-    {
+    if (config.source !== undefined) {
       this.source = config.source;
     }
 
     // Set the volume
-    if (config.volume !== undefined)
-    {
+    if (config.volume !== undefined) {
       this.volumeConnection.gain.value = config.volume;
     }
 
     // Set the pan
-    if (config.pan !== undefined)
-    {
+    if (config.pan !== undefined) {
       this.panConnection.setPosition(config.pan, 0, 0);
     }
 
     // Set looping
-    if (config.loop !== undefined)
-    {
+    if (config.loop !== undefined) {
       this.loop = config.loop;
     }
 
-    // Set a single instance of the sound
-    // If this is true, only one instance of the playSound bufferSource an be created.
-    // This prevents more than one instance of the sound playing at the same time
-    if (config.singleInstance !== undefined)
-    {
-      if (config.singleInstance)
-      {
+    /**
+     * Set a single instance of the sound. If this is true, only one instance 
+     * of the playSound bufferSource an be created. This prevents more than one 
+     * instance of the sound playing at the same time.
+     */
+    if (config.singleInstance !== undefined) {
+      if (config.singleInstance) {
         this.singleInstance = true;
         this.playSound = (function() {
           return audioContext.createBufferSource();
@@ -72,52 +69,41 @@ var soundObject = {
       }
     }
   },
-  play: function()
-  {
-    if (this.hasLoaded)
-    {
-      // create a playSound buffer source and determine whether only 1 or many instances
-      // of the same sound should play at the same time
-      var playsound;
-      if (this.singleInstance)
-      {
-        playSound = this.playSound;
-      }
-      else
-      {
-        playSound = audioContext.createBufferSource();
-      }
+  play: function() {
+    if (this.hasLoaded) {
+      // Create a playSound buffer source and determine whether only 1 or many 
+      // instances of the same sound should play at the same time.
+      var playSound = (this.singleInstance) ? this.playSound : audioContext.createBufferSource();
       playSound.buffer = this.buffer;
       playSound.connect(this.panConnection);
       this.panConnection.connect(this.volumeConnection);
       this.volumeConnection.connect(audioContext.destination);
-      // loop the sound
       playSound.loop = this.loop;
-      // Without volume connection:
-      // playSound.connect(audioContext.destination);
-      // (Volume should be the final output if you connect other effects)
+      /*
+       * Without volume connection:
+       * playSound.connect(audioContext.destination);
+       * Volume should be the final output if you connect other effects)
+       */
       playSound.noteOn(0);
     }
   },
-  stop: function()
-  {
+  stop: function() {
     // Disconnect the sound to stop it
     this.volumeConnection.disconnect();
   },
-  resume: function()
-  {
+  resume: function() {
     // Reconnect the sound to the panConnection to resume it
     this.volumeConnection.connect(audioContext.destination);
   },
-  setVolume: function(value)
-  {
+  setVolume: function(value) {
     this.volumeConnection.gain.value = value;
   },
-  setPan: function(value)
-  {
-    // Panner objects can accepnt x, y and z coordinates for 3D sound,
-    // but we're only interested in the the x coordinate, the first one,
-    // for left and right panning
+  setPan: function(value) {
+    /**
+     * Panner objects can accepnt x, y and z coordinates for 3D sound, 
+     * but we're only interested in the the x coordinate, 
+     * the first one, for left and right panning.
+     */
     this.panConnection.setPosition(value, 0, 0);
   }
 };
